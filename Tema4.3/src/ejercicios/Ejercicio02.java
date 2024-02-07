305fd178618c978942dfcdbf2a4a439a16aa8abf
jpgonzalez <jpgonzalez@106-03.info2>
2024-02-07 08:46:40 +0100

    Corregir 10

--- Tema4.3/src/ejercicios/Ejercicio02.java
+++ Tema4.3/src/ejercicios/Ejercicio02.java
@@ -34,28 +34,42 @@ public class Ejercicio02 {
 				// Le pedimos al usuario la nota que quiere introducir
 				System.out.println("Dime la " + aux + "º" + " nota:");
 				nota = sc.nextInt();
+
+				// En caso de que la nota sea menor a 0 o mayor de 10, mostramos un mensaje de
+				// error y peidmos el valor otra vez
 				if (nota > 10 || nota < 0) {
 					System.err.println("Error con la nota");
 					System.out.println("Dime la " + aux + "º" + " nota:");
 					nota = sc.nextInt();
 				}
+				// En caso de que sea correcto se introduce el valor introducido en la posición
+				// correcta
 				notas[i][j] = nota;
 				aux++;
 			}
 		}
 
+		// Inicializamos la variable auxiliar a 1 para empezar a contar los alumnos
 		aux = 1;
+
+		// Mostramos el número del alumno y sus notas usando un bucle for
 		for (int i = 0; i < 4; i++) {
 			System.out.println("Alumno " + aux + ": " + Arrays.toString(notas[i]));
 			aux++;
 		}
 
+		// Volvemos a inicializar la variable auxiliar a 1 para saber el número del
+		// alumno
 		aux = 1;
+
+		// Usamos un bucle for-each, creando un array donde vamos metiendole las notas
+		// de cada alumno
 		for (int[] alumnoNotas : notas) {
 			max = alumnoNotas[0];
 			min = alumnoNotas[0];
 			med = 0;
 
+			// Usamos otro for-each 
 			for (int notaAlumno : alumnoNotas) {
 				med += notaAlumno;
 				if (notaAlumno > max) {
---
+++ Tema4.3/src/ejercicios/Ejercicio10.java
@@ -0,0 +1,36 @@
+package ejercicios;
+
+public class Ejercicio10 {
+	
+	public static void main(String[] args) {
+		int tabla[][] = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}};
+		int tablaFin[][] = gira90(tabla);
+		for(int[] fila: tablaFin) {
+			for(int valor: fila) {
+				System.out.print(valor + "\t");
+			}
+			System.out.println();
+		}
+		
+	}
+
+	public static int[][] gira90(int tablaInicio[][]) {
+		int[][] tablaFinal = null;
+
+		if (tablaInicio != null) {
+			if (tablaInicio.length == tablaInicio[0].length) {
+				tablaFinal = new int[tablaInicio.length][tablaInicio.length];
+
+				for (int i = 0; i < tablaInicio.length; i++) {
+					for (int j = 0; j < tablaInicio[0].length; i++) {
+						tablaFinal[j][tablaInicio.length - i - 1] = tablaInicio[i][j];
+
+					}
+				}
+			}
+		}
+
+		return tablaFinal;
+	}
+
+}
